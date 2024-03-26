b747236ad5905bda1da8a811020ea73baba0336b
Suraj Kumar <kumarsuraj345678@gmail.com>
2024-03-26 18:43:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,80 @@
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#resetBtn");
-let newGameBtn = document.querySelector("#newGameBtn");
-let msgContainer = document.querySelector(".msgContainer");
-let msg = document.querySelector("#msg");
+document.addEventListener('DOMContentLoaded', function () {
+    const boxes = document.querySelectorAll(".box");
+    const resetBtn = document.querySelector("#resetBtn");
+    const newGameBtn = document.querySelector("#newGameBtn");
+    const msgContainer = document.querySelector(".msgContainer");
+    const msg = document.querySelector("#msg");
 
-let turnO = true;
-let clickCount = 0;
+    let turnO = true;
+    let clickCount = 0;
 
-const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8],
-];
+    const winPatterns = [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8],
+    ];
 
-const resetGame = () => {
-    turnO = true;
-    clickCount=0;
-    enableBoxes();
-    msgContainer.classList.add("hide");
-};
+    function resetGame() {
+        turnO = true;
+        clickCount = 0;
+        enableBoxes();
+        msgContainer.classList.add("hide");
+    }
 
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        if (turnO) {
-            box.innerText = "O";
-            turnO = false;
-            box.classList.add("red");
-            box.classList.remove("blue");
-        } else {
-            box.innerText = "X";
-            turnO = true;
-            box.classList.add("blue");
-            box.classList.remove("red");
+    function togglePlayer() {
+        turnO = !turnO;
+    }
+
+    function handleClick(box) {
+        if (!box.textContent) {
+            box.textContent = turnO ? "O" : "X";
+            box.classList.add(turnO ? "red" : "blue");
+            togglePlayer();
+            box.disabled = true;
+            clickCount++;
+            checkWinner();
         }
-        box.disabled = true;
-        clickCount++;
-        checkWinner();
-    });
-});
-
-const disableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = true;
     }
-};
 
-const enableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = false;
-        box.innerText = "";
+    function disableBoxes() {
+        boxes.forEach(box => box.disabled = true);
     }
-};
 
-const showWinner = (winner) => {
-    msg.innerText = `Congratulations! Winner is ${winner}`;
-    msgContainer.classList.remove("hide");
-    disableBoxes();
-};
+    function enableBoxes() {
+        boxes.forEach(box => {
+            box.disabled = false;
+            box.textContent = "";
+        });
+    }
 
-const showDraw = () => {
-    msg.innerText = "It's a draw! No winner this time.";
-    msgContainer.classList.remove("hide");
-    disableBoxes();
-};
+    function showResult(message) {
+        msg.textContent = message;
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+    }
 
-const checkWinner = () => {
-    for (let pattern of winPatterns) {
-        let pos1Val = boxes[pattern[0]].innerText;
-        let pos2Val = boxes[pattern[1]].innerText;
-        let pos3Val = boxes[pattern[2]].innerText;
-
-        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-            if (pos1Val === pos2Val && pos2Val === pos3Val) {
-                showWinner(pos1Val);
+    function checkWinner() {
+        for (let pattern of winPatterns) {
+            let [pos1, pos2, pos3] = pattern;
+            let pos1Val = boxes[pos1].textContent;
+            let pos2Val = boxes[pos2].textContent;
+            let pos3Val = boxes[pos3].textContent;
+            if (pos1Val && pos1Val === pos2Val && pos1Val === pos3Val) {
+                showResult(`Congratulations! Winner is ${pos1Val}`);
                 return;
             }
         }
+        if (clickCount === 9) {
+            showResult("It's a draw! No winner this time.");
+        }
     }
-    if (clickCount===9) {
-        showDraw();
-    }
-};
 
-newGameBtn.addEventListener("click", resetGame);
-resetBtn.addEventListener("click", resetGame);
+    boxes.forEach(box => box.addEventListener("click", () => handleClick(box)));
+    newGameBtn.addEventListener("click", resetGame);
+    resetBtn.addEventListener("click", resetGame);
+});
